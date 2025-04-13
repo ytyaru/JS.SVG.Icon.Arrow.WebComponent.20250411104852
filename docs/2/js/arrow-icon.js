c15c1362af873a289f5432e5da4ed63967577703
ytyaru <yttry0@gmail.com>
2025-04-13 09:33:51 +0900

    2 stroke-width等も可変にする

--- docs/2/js/arrow-icon.js
+++ docs/2/js/arrow-icon.js
@@ -1,11 +1,14 @@
 class ArrowIconElement extends HTMLElement {
-    static get observedAttributes() {return 'sz dir col'.split(' ');}
+    static get observedAttributes() {return 'sz dir col wid join limit'.split(' ');}
     constructor() {
         super();
         this._id = `icon-symbol-arrow`;
         this._sz = `1em`;
-        this._col= `currentColor`;
+        this._col = `currentColor`;
         this._dir = `0`;
+        this._wid = `16`;
+        this._join = `miter`; // arcs|bevel|miter|miter-clip|round
+        this._limit = `4`;
         if (!document.querySelector(`#${this._id}`)) {document.querySelector('head').append(this.#getArrowSymbol())}
     }
     connectedCallback() {
@@ -21,6 +24,10 @@ class ArrowIconElement extends HTMLElement {
         let attrs = {}
              if ('sz'===name) {this._sz = newValue}
         else if ('dir'===name) {this._dir = newValue}
+        else if ('col'===name) {this._col = newValue}
+        else if ('wid'===name) {this._wid = newValue}
+        else if ('join'===name) {this._join = newValue}
+        else if ('limit'===name) {this._limit = newValue}
         if (this.children && this.children[0]) {this.children[0].replaceWith(this.#mkEl())}
     }
     #mkEl() {return this.#getUse(this.#getSize(this._sz), this.#getTransform(this._dir))}
@@ -53,6 +60,10 @@ class ArrowIconElement extends HTMLElement {
         }
         use.setAttribute('href', `#${this._id}`);
         use.setAttribute('fill', this._col);
+        use.setAttribute('stroke', this._col);
+        use.setAttribute('stroke-width', this._wid);
+        use.setAttribute('stroke-linejoin', this._join);
+        use.setAttribute('stroke-miterlimit', this._limit);
 //        use.setAttribute('stroke', this._col);
         svg.setAttribute('viewBox', `0 0 256 256`);
         svg.append(use);
@@ -60,12 +71,17 @@ class ArrowIconElement extends HTMLElement {
     }
     #getArrowSymbol() {
 //<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path style="opacity:1;fill:none;fill-opacity:0;stroke:currentColor;stroke-width:16;stroke-linecap:square;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" d="M121.273 6.725 6.727 128l114.546 121.275v-79.79h128.211v-82.97h-128.21V6.726z"/></svg>
+//<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path d="M120.137 19.176 11.314 128l108.823 108.824V176H248V80H120.137V19.176z" style="pointer-events:all;opacity:1;fill:none;fill-opacity:0;stroke:currentColor;stroke-width:16;stroke-linecap:square;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/></svg>
         const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
         const defs = document.createElementNS('http://www.w3.org/2000/svg', 'defs');
         const symbol = document.createElementNS('http://www.w3.org/2000/svg', 'symbol');
         const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-        path.setAttribute('style', `pointer-events:all;opacity:1;fill:none;fill-opacity:0;stroke:currentColor;stroke-width:16;stroke-linecap:square;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`);
-        path.setAttribute('d', 'M121.273 6.725 6.727 128l114.546 121.275v-79.79h128.211v-82.97h-128.21V6.726z')
+//        path.setAttribute('style', `pointer-events:all;opacity:1;fill:none;fill-opacity:0;stroke:currentColor;stroke-width:${this._wid};stroke-linecap:square;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`);
+//        path.setAttribute('d', 'M121.273 6.725 6.727 128l114.546 121.275v-79.79h128.211v-82.97h-128.21V6.726z')
+        //path.setAttribute('style', `pointer-events:all;opacity:1;fill:none;fill-opacity:0;stroke:currentColor;stroke-width:${this._wid};stroke-linecap:square;stroke-linejoin:${this._join};stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1`)
+        path.setAttribute('style', `pointer-events:all;opacity:1;fill-opacity:0;stroke-linecap:square;stroke-dasharray:none;stroke-opacity:1`)
+        path.setAttribute('d', 'M120.137 19.176 11.314 128l108.823 108.824V176H248V80H120.137V19.176z')
+        
         symbol.setAttribute('viewBox', '0 0 256 256')
         symbol.setAttribute('id', this._id)
         symbol.append(path)
